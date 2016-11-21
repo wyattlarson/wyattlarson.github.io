72eedced59b67ca39c342bac5e6390b32ae59dcf
wyattlarson <wyattlarson250@gmail.com>
2016-11-20 23:29:13 -0700

    weathe api

---
+++ homework/jsonweather/modules/navigation.php
@@ -0,0 +1,6 @@
+<ul class="flexlist">
+	<li><a href="/jsonweather/" title="Weather Center Current Location">Current</a></li>
+	<li><a href="/jsonweather/views/franklin.php" title="Weather in Franklin, ID">Franklin</a></li>
+	<li><a href="/jsonweather/views/greenville.php" title="Weather in Greenville, SC">Greenville</a></li>
+	<li><a href="/jsonweather/views/springfield.php" title="Weather in Springfield, IL">Springfield</a></li>
+</ul>
