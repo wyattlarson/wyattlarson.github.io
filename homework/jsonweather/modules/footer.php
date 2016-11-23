0aad561ab67d8ba076c58d85e2f5730e5a9a4f3b
wyattlarson <wyattlarson250@gmail.com>
2016-11-22 21:08:45 -0700

    location initial

--- homework/jsonweather/modules/footer.php
+++
@@ -1,4 +0,0 @@
-<ul class="flexlist">
-	<li>Weather Center 2010 - <?php echo date('Y');  ?></li>
-	<li><img src="/jsonweather/images/wulogo_rev.png" alt="Weatherunderground logo" title="All weather data provided by WeatherUnderground.com"></li>
-</ul>
---
+++ jsonweather-html/scripts/remote_wu.js
@@ -0,0 +1,51 @@
+// Current Location Scripts
+$(function () {
+
+  var status = $('#status');
+
+  (function getGeoLocation() {
+    status.text('Getting Location...');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        // Call the getData function, send the lat and long
+        getData(lat, long);
+
+      });
+    } else {
+      status.text("Your browser doesn't support Geolocation or it is not enabled!");
+    }
+
+  })();
+
+  // Get the data from the wunderground API
+  function getData(lat, long){
+    $.ajax({
+            url : "http://api.wunderground.com/api/11fc9ae6852b90d4/geolookup/conditions/q/"+lat+','+long+".json",
+            dataType : "jsonp",
+            success : function(data) {
+                var location = data['location']['city'];
+                var temp_f = Math.round(data['current_observation']['temp_f']);
+                var weather = data['current_observation']['weather'];
+                var feelslike = Math.round(data['current_observation']['feelslike_f']);
+                var state = data['location']['state'];
+                console.log(location + ", " + temp_f + ": " + state);
+                $("#cityDisplay").html(location + ", " + state);
+                $("#currentTemp").html(temp_f + "°");
+                $("#summary").html(weather);
+                $("#feelslike").html("Feels like it is: " + feelslike + "°");
+              }
+
+
+
+    });
+       $("#cover").fadeOut(250);
+  };
+
+  // A function for changing a string to TitleCase
+  function toTitleCase(str){
+    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+});
