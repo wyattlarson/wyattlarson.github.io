0d277492d13e19c3a329ffbb0dcdcc96d174edcf
wyattlarson <wyattlarson250@gmail.com>
2016-11-28 09:08:23 -0700

    https update

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -23,7 +23,7 @@ $(function () {
   // Get the data from the wunderground API
   function getData(lat, long){
     $.ajax({
-            url : "http://api.wunderground.com/api/11fc9ae6852b90d4/geolookup/conditions/q/"+lat+','+long+".json",
+            url : "https://api.wunderground.com/api/11fc9ae6852b90d4/geolookup/conditions/q/"+lat+','+long+".json",
             dataType : "jsonp",
             success : function(data) {
                 var location = data['location']['city'];
