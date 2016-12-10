cb8bf70de244847393b780c1b02cdc085f646ed8
wyattlarson <wyattlarson250@gmail.com>
2016-12-10 13:06:30 -0700

    update

--- modules/pricing.js
+++ modules/pricing.js
@@ -1,15 +1,3 @@
- $(function getData(input) {
-
-
- $.ajax({
-    url: "https://wyattlarson.github.io/modules/pricing.json"
-    , dataType: "jsonp"
-    , success: function (data) {
-     console.log('data');
-//      var month = data.pricing.monthly;
-//      var bi = data.pricing.biyearly;
-//      var yearly = data.pricing.yearly;
-//      console.log('month + bi + yearly');
-    }
-  });
-});
+$.getJSON( "pricing.json", function( json ) {
+  console.log( "JSON Data: " + json.users[ 3 ].name );
+ });
