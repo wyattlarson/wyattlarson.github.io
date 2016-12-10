cb053ca768cb11e0455656c304fc729a6a87568e
wyattlarson <wyattlarson250@gmail.com>
2016-12-10 12:51:51 -0700

    json update

--- modules/pricing.js
+++ modules/pricing.js
@@ -1,14 +1,15 @@
- $(function() {
+ $(function getData(input) {
+
 
  $.ajax({
     url: "https://wyattlarson.github.io/modules/pricing.js"
     , dataType: "jsonp"
     , success: function (data) {
-      console.log(data);
-      var month = data.pricing.monthly;
-      var bi = data.pricing.biyearly;
-      var yearly = data.pricing.yearly;
-      console.log('month + bi + yearly');
+     console.log('data');
+//      var month = data.pricing.monthly;
+//      var bi = data.pricing.biyearly;
+//      var yearly = data.pricing.yearly;
+//      console.log('month + bi + yearly');
     }
   });
 });
