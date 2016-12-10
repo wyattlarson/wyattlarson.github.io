88744a907bf05afd790f2d2ec5d7bdad90628c41
wyattlarson <wyattlarson250@gmail.com>
2016-12-10 13:35:06 -0700

    privacy.json commit

--- modules/pricing.js
+++ modules/pricing.js
@@ -1,3 +1,9 @@
-$.getJSON( "https://wyattlarson.github.io/modules/pricing.json", function( json ) {
-  console.log( "JSON Data: " + json.users[ 3 ].name );
+$.getJSON( "https://wyattlarson.github.io/modules/pricing.json", function(json) {
+ var bi = json["biyearly"];
+  var month = json["monthly"];
+  var year = json["yearly"];
+  console.log( json );
+  $("#monthlyPrice").html(month);
+  $('#bimonth').html(bi);
+  $('#year').html(year);
  });
